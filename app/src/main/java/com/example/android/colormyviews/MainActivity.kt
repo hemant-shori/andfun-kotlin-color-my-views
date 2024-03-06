bdf0d36c3ac1f914a86b72b6536e25d069b41d70
Hemant Shori <hemantshori93@gmail.com>
2024-03-06 01:47:51 +0100

    Align box and add click handlers using ComposeUI
    Step.02-Solution-Align-boxes-Click-handlers

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -4,23 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +40,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.outline
                 ) {
                     RootView()
                 }
@@ -46,10 +51,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun RootView(modifier: Modifier = Modifier) {
+    val listOfColor = listOf(
+        Color.DarkGray,
+        Color.Gray,
+        colorResource(android.R.color.holo_green_light),
+        colorResource(android.R.color.holo_green_dark),
+        colorResource(android.R.color.holo_green_light)
+    )
+    val mutableColorList = remember {
+        mutableStateListOf<Color>().apply {
+            repeat(5) {
+                add(Color.Transparent)
+            }
+        }
+    }
     Column(
-        modifier = Modifier.fillMaxHeight()
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(Color.LightGray)
     ) {
-
         Text(
             text = stringResource(id = R.string.box_one),
             textAlign = TextAlign.Center,
@@ -61,9 +81,120 @@ fun RootView(modifier: Modifier = Modifier) {
                 .fillMaxWidth()
                 .padding(20.dp)
                 .width(IntrinsicSize.Max)
-                // set the background color to the Max Width size
-                .background(colorResource(id = R.color.my_green)),
+                .background(mutableColorList[0])
+                .clickable(
+                    onClick = {
+                        val index = 0
+                        mutableColorList[index] = listOfColor[index]
+                    }
+                )
         )
+        // Box == FrameLayout which is required to make IntrinsicSize.Max work
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max)
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = modifier
+                        .align(alignment = Alignment.CenterVertically)
+                        .fillMaxHeight()
+                        .padding(
+                            start = 16.dp
+                        )
+                        .weight(1f)
+                        .background(mutableColorList[1])
+                        .clickable(
+                            onClick = {
+                                val index = 1
+                                mutableColorList[index] = listOfColor[index]
+                            }
+                        )
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.box_two),
+                        // set the text color
+                        color = colorResource(id = android.R.color.white),
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(2f)
+                        .height(IntrinsicSize.Min)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.box_three),
+                        textAlign = TextAlign.Center,
+                        // set the text color
+                        color = colorResource(id = android.R.color.white),
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.Bold,
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(
+                                start = 16.dp,
+                                end = 16.dp
+                            )
+                            .background(mutableColorList[2])
+                            .clickable(
+                                onClick = {
+                                    val index = 2
+                                    mutableColorList[index] = listOfColor[index]
+                                }
+                            )
+                    )
+                    Text(
+                        text = stringResource(id = R.string.box_four),
+                        textAlign = TextAlign.Center,
+                        // set the text color
+                        color = colorResource(id = android.R.color.white),
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.Bold,
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(
+                                start = 16.dp,
+                                top = 8.dp,
+                                end = 16.dp
+                            )
+                            .background(mutableColorList[3])
+                            .clickable(
+                                onClick = {
+                                    val index = 3
+                                    mutableColorList[index] = listOfColor[index]
+                                }
+                            )
+                    )
+
+                    Text(
+                        text = stringResource(id = R.string.box_five),
+                        textAlign = TextAlign.Center,
+                        // set the text color
+                        color = colorResource(id = android.R.color.white),
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.Bold,
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(
+                                start = 16.dp,
+                                top = 8.dp,
+                                end = 16.dp
+                            )
+                            .background(mutableColorList[4])
+                            .clickable(
+                                onClick = {
+                                    val index = 4
+                                    mutableColorList[index] = listOfColor[index]
+                                }
+                            )
+                    )
+                }
+            }
+        }
     }
 }
 
